120a315e4e181bf051b9da061590c4cbb9be65da
Komandes <72608602+Komandes@users.noreply.github.com>
2021-09-13 14:25:14 +0200

    2

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.all('/secret', function(req, res, next) {
+  console.log('Accessing the secret section ...');
+  next(); // pass control to the next handler
 });
+
+const wiki = require('./wiki.js');
+// ...
+app.use('/wiki', wiki);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
